c9a7c116f49b1dd643b20c7871480f55bf906618
thibault <thibault.vanwynendaele@live.fr>
2018-03-16 22:16:44 +0100

    init backend

---
+++ Backend/src/test/java/lu/adneom/place2be/Place2beApplicationTests.java
@@ -0,0 +1,16 @@
+package lu.adneom.place2be;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Place2beApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
